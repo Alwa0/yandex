00c1d519cb9af4d69c262b0fb538d2b761693a18
alwa0 <a.paukova@innopolis.university>
2021-03-29 09:22:15 +0300

    delete trailing slash

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('orders/assign/', views.assign, name='assign orders'),
-    path('orders/complete/', views.complete, name='complete order'),
-    path('couriers/', views.couriers, name='post couriers'),
+    path('orders/assign', views.assign, name='assign orders'),
+    path('orders/complete', views.complete, name='complete order'),
+    path('couriers', views.couriers, name='post couriers'),
     path('couriers/<int:courier_id>', views.edit, name='courier'),
-    path('orders/', views.orders, name='post orders'),
+    path('orders', views.orders, name='post orders'),
 ]
